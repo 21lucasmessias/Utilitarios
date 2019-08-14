ec36ce6a9c5bb4842e1d0ebfbe2a08682c26758c
Lucas Messias do Prado <41164252+21lucasmessias@users.noreply.github.com>
2019-08-14 13:47:09 -0300

    Add files via upload

---
+++ Calcular dias.py
@@ -0,0 +1,50 @@
+di = 14
+mi = 8
+ai = 2018
+df = 12
+mf = 8
+af = 2019
+cont = ai
+
+def dias(d, m, a):
+    dt = 0
+    if bissexto(a):
+        lm = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    else:
+        lm = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    for i in range(1, m):
+        dt += lm[i]
+    dt += d
+    return dt
+
+
+def bissexto(a):
+    if a % 4 == 0 and (a % 100 != 0 or a % 400 == 0):
+        return True
+    else:
+        return False
+
+
+if af < ai:
+    print('ano final < ano inicial')
+elif ai == af:
+    dd = dias(df, mf, af) - dias(di, mi, ai)
+else:
+    dd = 0
+    while True:
+        if cont == af:
+            dd += dias(df, mf, af)
+            break
+        elif cont == ai:
+            if bissexto(cont):
+                dd += 366 - dias(di, mi, ai)
+            else:
+                dd += 365 - dias(di, mi, ai)
+        elif cont < af:
+            if bissexto(cont):
+                dd += 366
+            else:
+                dd += 365
+        cont += 1
+
+print(dd)
